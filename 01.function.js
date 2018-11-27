b6a93bed521c3fd645502a22d0fc0d8127ee0202
stefOuroboros <stephane.gallois89@gmail.com>
2018-11-27 12:24:17 +0100

    01 Fonctions over

--- 01.function.js
+++ 01.function.js
@@ -4,12 +4,33 @@ var nombre1 = 10;
 var nombre2 = 20;
 
 function additionner(nb1, nb2) {
-    return nb1 + nb2;
+    return nb1+nb2;
 }
 
 var resultat1 = additionner(nombre1, nombre2);
 console.log(resultat1);
 
-var somme = additionner();
+var somme = additionner;
 var resultat2 = somme(nombre1, nombre2);
 console.log(resultat2);
+
+var multiplication = function(nb1, nb2) {
+    return nb1*nb2;
+}
+var resultat3 = multiplication(nombre1, nombre2);
+console.log(resultat3);
+
+var afficherOperation = function(nomOperation, operation, nb1, nb2) {
+    console.log(nomOperation+"("+nb1+", "+nb2+") = "+operation(nb1, nb2));
+}
+
+afficherOperation("Somme", somme, 20, 40);
+afficherOperation("Multiplication", multiplication, 10, 20);
+// afficherOperation("Soustraction", function(nb1, nb2) {
+//     return nb1 - nb2;
+// }, 15, 5 );
+
+// afficherOperation("Soustraction", (nb1, nb2) => {
+//     return nb1 - nb2;
+// }, 15, 5 );
+afficherOperation("Soustraction", (nb1, nb2) =>  nb1 - nb2, 15, 5 );
