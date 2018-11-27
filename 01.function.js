088583d0e5f8f7c57771e6b408e90d59d16d7b5b
Champetier <todave11@gmail.com>
2018-11-27 12:15:12 +0100

    Fonction comme élément du 1er ordre

--- 01.function.js
+++ 01.function.js
@@ -17,11 +17,19 @@ var somme = additionner;
 var resultat2 = somme(nombre1,nombre2);
 cl("resultat2 =",resultat2);
 
-var multiplication = function(nb1,nb2){
-    return nb1*nb2;
-}
+// var multiplication = function(nb1,nb2){
+//     return nb1*nb2;
+// }
 
-// var multiplicationl =(nb1,nb2)=>nb1*nb2;
+var multiplication =(nb1,nb2)=>nb1*nb2;
 
 var resultat3=multiplication(nombre1,nombre2);
 cl("resultat3 =",resultat3)
+
+var afficherOperation =function(nomOperation,operation,nb1,nb2){
+    console.log(nomOperation,"(",nb1," , ", nb2,") = ",operation(nb1,nb2));
+}
+
+afficherOperation("Somme",somme,20,40);
+afficherOperation("Multiplication",multiplication,10,20);
+afficherOperation("Soustraction",(nb1,nb2)=>nb1-nb2,15,5);
