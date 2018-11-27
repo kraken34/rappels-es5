c10a778fc0c91b91996fca63ac442c7e53a3ca80
stefOuroboros <stephane.gallois89@gmail.com>
2018-11-27 14:40:15 +0100

    02.tableaux.js over

--- 02.tableaux.js
+++ 02.tableaux.js
@@ -1,3 +1,8 @@
 var villes = ['nantes', 'paris', 'saint-nazaire', 'angers', 'le mans'];
-villes.forEach((element)=>console.log(element));
-// villes.every()
+villes.forEach((ville)=>console.log(ville));
+villes.every((ville)=>console.log("lettre 'a' dans toutes les villes = "+ville.includes('a')));
+console.log("Au moins une ville avec un tiret = "+villes.some((ville)=>ville.includes('-')));
+var villesSansTiretSansEspace = villes.filter((ville)=>!ville.includes('-')&& !ville.includes(' '));
+console.log(villesSansTiretSansEspace);
+var villesMajusculeSeTerminantParS = villes.map((ville)=>ville.toUpperCase()).filter((ville)=>ville.endsWith('S')); // Inverser le tri et le UpperCase serait plus logique
+console.log(villesMajusculeSeTerminantParS);
