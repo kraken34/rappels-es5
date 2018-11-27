26f44094e2370ba750ad07cd74eb4ee1ce409694
Bastianelli_Matthieu <m.bastianelli@laposte.net>
2018-11-27 14:21:11 +0100

    Fin tp tableaux js

--- 02.tableaux.js
+++ 02.tableaux.js
@@ -5,5 +5,21 @@ villes = ['nantes', 'paris', 'saint-nazaire', 'angers', 'le mans'];
 villes
     .forEach(ville => lg(ville));
 
+lettreADansToutesLesVilles = villes.every(ville => ville.includes('a'));
+lg('lettreADansToutesLesVilles = '+lettreADansToutesLesVilles);    
 
-lg('lettreADansToutesLesVilles = '+villes.every(ville => ville.includes('a')));    
+auMoinsUneVilleAvecUnTiret = villes.some((ville) => ville.includes('-'));
+lg('auMoinsUneVilleAvecUnTiret = '+auMoinsUneVilleAvecUnTiret);
+
+villesSansTiretSansEspace = villes.filter(ville => (!ville.includes(' '))&&(!ville.includes('-')) );
+
+lg('villesSansTiretSansEspace : ');
+lg(villesSansTiretSansEspace);
+
+villesMajusculeSeTerminantParS = villes
+                                        .map(ville => ville = ville.toUpperCase())
+                                        .map(ville=> !ville.endsWith('S')? ville+'S' :ville);
+
+lg('villesMajusculeSeTerminantParS :');
+lg(villesMajusculeSeTerminantParS);                                        
+                                        
