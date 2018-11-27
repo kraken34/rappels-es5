31c4ccd511eb69267169fea880260392aa06ab60
Champetier <todave11@gmail.com>
2018-11-27 13:50:08 +0100

    Partie 2 - Les tableaux
    fini

--- 02.tableaux.js
+++ 02.tableaux.js
@@ -1,7 +1,16 @@
+function cl(message ,affichage){
+    console.log(message,affichage);
+}
 var villes=["nantes","paris","saint-nazaire","angers","le mans"];
 villes.forEach(function(lieu){
     console.log(lieu);
 })
 
 var lettreADansToutesLesVilles =villes.every((lieu)=>lieu.includes('a'))
-console.log(lettreADansToutesLesVilles);
+cl("lettreADansToutesLesVilles =",lettreADansToutesLesVilles);
+
+var auMoinsUneVilleAvecUnTiret=villes.some((lieu)=>lieu.includes("-"))
+cl("auMoinsUneVilleAvecUnTiret =",auMoinsUneVilleAvecUnTiret)
+
+var villesMajusculeSeTerminantParS=villes.filter((lieu)=>lieu.charAt(lieu.length-1)=='s').map((lieu)=>lieu.toUpperCase())
+cl("villesMajusculeSeTerminantParS =",villesMajusculeSeTerminantParS)
